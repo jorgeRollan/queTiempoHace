c58657622eb9bbcd5f6708db400c103c14886fbb
jorge <jorgerollandiez@gmail.com>
2024-10-09 10:11:01 +0200

    importada la librerira compass cardinal para convertir los grados del viento a direccion cardinal, uso dos funciones una de grado a direccion en siglas y de siglas a nombre entero

--- src/components/ShowWeather.jsx
+++ src/components/ShowWeather.jsx
@@ -1,22 +1,19 @@
-import { Suspense } from "react";
+import * as Compass from 'cardinal-direction';
 
 const ShowWeather = (props) =>{
   let {datos : {name, main:{temp, feels_like,humidity,pressure}, wind:{deg,speed}, weather}} = props
-  name = null;
   return (
-    <Suspense fallback={<h3>No disponible</h3>}>
     <div id="datosDefecto">
-      <h2>Clima en {name ?? "nombre no disponible"}</h2>
+      <h2>Clima en {name ?? "No disponible" }</h2>
       <p><strong>Temperatura:</strong> {temp ?? "temperatura no disponible"} °C</p>
       <p><strong>Sensación Térmica:</strong> {feels_like ?? "sensación termica no disponible"} °C</p>
       <img src={`http://openweathermap.org/img/w/${weather[0].icon}.png`} alt="wthr img" />
       <p><strong>Humedad:</strong> {humidity ?? "humedad no disponible"} %</p>
       <p><strong>Descripción:</strong> {weather[0].description ?? "descripción no disponible"}</p>
-      <p><strong>Direccion del viento:</strong> {deg ?? "dirección del viento no disponibles"}</p>
+      <p><strong>Direccion del viento:</strong> {Compass.cardinalConverter(Compass.cardinalFromDegree(deg)) ?? "dirección del viento no disponibles"}</p>
       <p><strong>Velocidad del viento:</strong> {speed ?? "velocidad del viento no disponible"}</p>
       <p><strong>Presion atmosférica:</strong> {pressure ?? "presión atmosférica no disponible"}</p>
     </div>
-    </Suspense>
   )
 }
 
