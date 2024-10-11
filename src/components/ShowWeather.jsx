2dfb60dc3be2791186b22147434497c0ea8f415d
jorge <jorgerollandiez@gmail.com>
2024-10-11 09:21:20 +0200

    corregido el fallback para que muestre unidades si no hay error

--- src/components/ShowWeather.jsx
+++ src/components/ShowWeather.jsx
@@ -8,14 +8,14 @@ const ShowWeather = () => {
   return (
     <div id="datosDefecto">
       <h2>Clima en {name ?? "No disponible"}</h2>
-      <p><strong>Temperatura:</strong> {temp !== null ? temp + "ºC" : "temperatura no disponible"}</p>
-      <p><strong>Sensación Térmica:</strong> {feels_like ?? "sensación termica no disponible"} °C</p>
-      <img src={`http://openweathermap.org/img/w/${weather[0].icon}.png`} alt="wthr img" />
-      <p><strong>Humedad:</strong> {humidity ?? "humedad no disponible"} %</p>
+      <p><strong>Temperatura:</strong> {temp ? `${Math.round(temp)} ºC` : "temperatura no disponible"}</p>
+      <p><strong>Sensación Térmica:</strong> {feels_like ? `${Math.round(feels_like)} ºC` : "temperatura no disponible"}</p>
+      <img src={`http://openweathermap.org/img/w/${weather[0].icon}.png`} alt="icono del tiempo no disponible" />
+      <p><strong>Humedad:</strong> {humidity ? `${humidity} %` : "humedad no disponible"}</p>
       <p><strong>Descripción:</strong> {weather[0].description ?? "descripción no disponible"}</p>
-      <p><strong>Direccion del viento:</strong> {Compass.cardinalConverter(Compass.cardinalFromDegree(deg)) ?? "dirección del viento no disponibles"}</p>
-      <p><strong>Velocidad del viento:</strong> {speed ?? "velocidad del viento no disponible"}</p>
-      <p><strong>Presion atmosférica:</strong> {pressure ?? "presión atmosférica no disponible"}</p>
+      <p><strong>Direccion del viento:</strong> {Compass.cardinalConverter(Compass.cardinalFromDegree(deg)) ?? "dirección del viento no disponible"}</p>
+      <p><strong>Velocidad del viento:</strong> {speed ? `${speed} KM/H` : "velocidad del viento no disponible"}</p>
+      <p><strong>Presion atmosférica:</strong> {pressure ? `${pressure} Bares` : "presión atmosférica no disponible"}</p>
     </div>
   )
 }
